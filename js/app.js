19c47d873215aed93f4784abdf5d05c56501753f
simretB05 <happysimret@gmail.com>
2023-04-02 14:43:39 -0600

    added back ground image to the body and styled the index page

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 //selected the the section where the pokes are displyed with queryselector
-let get_main = document.querySelector( `#main` );
+let main_section = document.querySelector( `.main_section` );
 
 for ( let i = 0; i <pokemon.length; i++ ){
-    get_main.insertAdjacentHTML( `beforeend`, 
-    `<div class="main">
+    main_section.insertAdjacentHTML( `beforeend`, 
+    ` <div class="main">
     <h2 class="name">${pokemon[i][`name`]} </h2>
-    <img src="${pokemon[i][`image_url`]}" alt="Pokémon image">
-    <button class="selection" selection_kind="Pikachu">Choose Me</button>
-     </div>
+    <img style="width:80px; height:80px; margin:30px; box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3); " src="${pokemon[i][`image_url`]}" alt="Pokémon image">
+    <a style="text-decoration:none; border-radius:10px; border:1px solid black; color:Black; background-color:white; padding:5px;"  href="/pages/battle_arena.html class="selection" selection_kind="Pikachu"> Choose Me</a>
+
+    </div>
   `)
 
 }
--- js/pokemon.js
+++ js/pokemon.js
@@ -5,8 +5,8 @@ let pokemon = [
     image_url:`/images/Pikachu.jpg`,
     attacks: [
         {
-        name:`Block`,
-        damage:7
+            name:`Block`,
+            damage:7
         },
         {
             name:`Blaze kick`,
@@ -17,7 +17,45 @@ let pokemon = [
             damage:7
         }
     ]
-    }
+    },
+    {
+    name: `Charizard`,
+    max_health: 50,
+    image_url:`/images/charizaed.jpg`,
+    attacks: [
+        {
+            name:`Defog`,
+            damage:7
+        },
+        {
+            name:`Egg Bomb`,
+            damage:7
+        },
+        {
+            name:`fire punch`,
+            damage:7
+        }
+    ]
+    },
+    {
+    name: `Eevee`,
+    max_health: 50,
+    image_url:`/images/evee.jpg`,
+    attacks: [
+        {
+            name:`Block`,
+            damage:7
+        },
+        {
+            name:`Blaze kick`,
+            damage:7
+        },
+        {
+            name:`Bulk up`,
+            damage:7
+        }
+    ]
+    },
 ]
 
 
