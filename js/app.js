eea089a08d8d78165a6e748cac2b1d54f14a8aee
simretB05 <happysimret@gmail.com>
2023-04-03 05:53:22 -0600

    fixed issue of loop selecting the same pokemon

--- js/app.js
+++ js/app.js
@@ -7,8 +7,8 @@ for ( let i = 0; i <pokemon.length; i++ ){
     main_section.insertAdjacentHTML( `beforeend`, 
     ` <div class="main">
     <h2 class="name">${pokemon[i][`name`]} </h2>
-    <img style="width:80px; height:80px; margin:30px;  " src="${pokemon[i][`image_url`]}" alt="Pokémon image">
-    <a style="text-decoration:none; border-radius:10px; background-color:white; padding:5px;"  class="selection" selection_kind="Pikachu"> Choose Me</a>
+    <img style="width:80px; height:80px; margin:30px; " src="${pokemon[i][`image_url`]}" alt="Pokémon image">
+    <a style="text-decoration:none; border-radius:10px; cursor: grab;; background-color:white; padding:5px;"  class="selection" selection_kind="${pokemon[i][`name`]}"> Choose Me</a>
 
     </div>
   `)
@@ -19,39 +19,33 @@ let userData = []
 let compData=[]
 function selectPokemon( details ){
     let userPokemon = undefined;
-    let selected_pokeman = details[`target`].getAttribute( `selection_kind` )
+    let selected_pokeman = details[`target`].getAttribute(`selection_kind`)
     for ( let x = 0; x < pokemon.length; x++ ){
         if ( selected_pokeman === pokemon[x][`name`] ){
             userPokemon = pokemon[x]
-            userData.push( pokemon[x])
-          
-            
         }
           
-        if ( !( selected_json === undefined ) ){ 
-            newArry = JSON.parse(selected_json);
-         
-        }
-  
-    let compPokemon =pokemon[1]
-    let selection_json = JSON.stringify(userData)
-    let userPokemon_json = JSON.stringify(userData )
-    let compPokemon_json = JSON.stringify(compPokemon)
-    Cookies.set( `user_hp`, userPokemon[`max_health`])
-    Cookies.set( `comp_hp`, compPokemon[`max_health`])
-    Cookies.set( `user_pokemon`, userPokemon_json)
-    Cookies.set( `computer`, compPokemon_json )
-    Cookies.set( `selected_pokemon`, selection_json)
-    
-        
+      
     }
+    if ( !( selected_json === undefined ) ){ 
+        userData = JSON.parse(selected_json);
+     
+    }
+userData.push( userPokemon)
+let compPokemon =pokemon[1]
+let selection_json = JSON.stringify(userData)
+let userPokemon_json = JSON.stringify(userData )
+let compPokemon_json = JSON.stringify(compPokemon)
+Cookies.set( `user_hp`, userPokemon[`max_health`])
+Cookies.set( `comp_hp`, compPokemon[`max_health`])
+Cookies.set( `user_pokemon`, userPokemon_json)
+Cookies.set( `computer`, compPokemon_json )
+Cookies.set( `selected_pokemon`, selection_json)
 
-
+    
 }
 
 let selection_btn = document.querySelectorAll( `.selection` )
-
-
 
 for ( let i = 0; i < selection_btn.length; i++ ){
     selection_btn[i].addEventListener( `click`, selectPokemon )
--- js/battle_arena.js
+++ js/battle_arena.js
@@ -12,7 +12,7 @@ for ( i = 0; i < selection_json.length; i++ )
         ` <div class="main">
 <h2 class="name">${ selection_json[i][`name`] } </h2>
 <img style="width:80px; height:80px; margin:30px;" src="${ selection_json[i][`image_url`] }" alt="Pokémon image">
-<button style="text-decoration:none; border-radius:10px; background-color:white; padding:5px;"  class="selection" selection_kind="Pikachu">${ selection_json[`attack`] } </button>
+<button style="text-decoration:none; border-radius:10px;  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3); background-color:white; padding:5px; width:90px"  class="selection" selection_kind="Pikachu">${ selection_json[i][`attacks`][i][`name`]} </button>
 <p id="user_hp">${ selection_json[i][`max_health`] }</p>
 </div>
 ` )
